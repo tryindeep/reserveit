25f71b8e777d1ac804d05ed826cc087c58d10e6e
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-07-09 23:33:45 +0530

    Add Movie CRUD API endpoints

--- apps/backend/controllers/movie.controller.ts
+++ apps/backend/controllers/movie.controller.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from "express";
-import z from "zod";
+import z, { success } from "zod";
 import { MovieService } from "../services/movie.service";
 
 const createMovieSchema = z.object({
@@ -13,9 +13,14 @@ const createMovieSchema = z.object({
   releaseStatus: z.enum(["RELEASED", "UPCOMING", "CANCELLED"]).default("RELEASED"),
 });
 
-type MovieControllerType = {
-    createMovie: RequestHandler;
+const updateMovieSchema = createMovieSchema.partial();
 
+type MovieControllerType = {
+    createMovie: RequestHandler,
+    getAllMovies: RequestHandler,
+    getMovieById: RequestHandler,
+    updateMovie: RequestHandler,
+    deleteMovie: RequestHandler,
 }
 
 export const MovieController : MovieControllerType = {
@@ -47,4 +52,100 @@ export const MovieController : MovieControllerType = {
             })
         }
     },
+    
+    // GET ALL MOVIES
+    getAllMovies : async ( req, res) => {
+       try {
+            const movies = await MovieService.getAllMovies();
+            return res.status(200).json({
+                success: true,
+                data: movies
+            });
+       } catch (error) {
+        console.error("getAllMovies : ", error);
+        return res.status(500).json({
+            success: false,
+            message : "Internal server error"
+        })
+       }
+    },
+
+    // GET MOVIE BY ID 
+    getMovieById : async (req , res) => {
+        try {
+            const { id } = req.params;
+            const movie = await MovieService.getMovieById(id);
+            if(!movie){
+                return res.status(404).json({
+                    success: false, 
+                    message: "Movie not found"
+                });
+            }
+            return res.status(200).json({
+                success : true,
+                data : movie
+            })
+        } catch (error) {
+            console.error("getMovieById : ", error);
+            return res.status(500).json({
+                success: false,
+                message : "Internal server error"  });
+        }
+    },
+
+    // update
+    updateMovie :  async (req , res) => {
+        try {
+            const { id } = req.params;
+            
+            const parsed = updateMovieSchema.safeParse(req.body);
+            if(!parsed.success) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Invalid Input",
+                    errors: parsed.error.issues
+                });
+            }
+            const updatedMovie = await MovieService.updateMovie(id , parsed.data)
+            if(!updatedMovie){
+                return res.status(404).json({
+                    success: false,
+                    message : "Movie not found"
+                })
+            }
+            return res.status(200).json({success : true, data: updatedMovie})
+        } catch (error) {
+            console.error("updateMovie : ", error);
+            return res.status(500).json({
+                success: false,
+                message : "Internal server error"  });
+        }
+    },
+
+    // Delete Movie
+    deleteMovie : async (req , res) => {
+        try {
+            const { id } = req.params;
+            const deleted = await MovieService.deleteMovie(id);
+            if(!deleted){
+                return res.status(200).json({
+                    success: false, 
+                    message: "Movie not found"
+                })
+
+            }
+            return res.status(200).json({
+                success : true,
+                data : "Movie deleted success fully"
+            })
+            
+        } catch (error) {
+            console.error("deleteMovie : ", error);
+            return res.status(500).json({
+                success: false,
+                message : "Internal server error"  });
+        }
+        
+
+    }
 }
--- apps/backend/services/movie.service.ts
+++ apps/backend/services/movie.service.ts
@@ -4,4 +4,18 @@ export const MovieService = {
     createMovie : async (data : Prisma.MovieCreateInput ) => {
         return db.movie.create({data})
     },
+    getAllMovies : () => {
+        return db.movie.findMany({orderBy : {createdAt : "desc"}})
+    },
+    getMovieById : async (id : string) => {
+        return db.movie.findUnique({where :{id} })
+    },
+    updateMovie : async ( id : string , data : Prisma.MovieUpdateInput) => {
+        const existingMovie = await db.movie.findUnique({where: {id}});
+        if(!existingMovie) return null;
+        return db.movie.update({where:{id} , data});
+    },
+    deleteMovie : async(id:string) => {
+      return db.movie.delete({where:{id}});
+    }
 }
